695bba568c4f4ceaad42e8e0318fdf81b7016635
MarkusAleksander <markholden1989@googlemail.com>
2022-04-09 13:45:38 +0100

    Added firebase endpoints

---
+++ src/API/firebase.ts
@@ -0,0 +1,11 @@
+/**
+ * * Firebase API endpoints
+ */
+
+const BASE_URL: string = `https://music-library-4d53f-default-rtdb.europe-west1.firebasedatabase.app`;
+
+/*** ---- * ALBUMS ---- ***/
+export const ALBUMS = `${BASE_URL}/albums`;
+
+/*** ---- * ARTISTS ---- ***/
+export const ARTISTS = `${BASE_URL}/artists`;
