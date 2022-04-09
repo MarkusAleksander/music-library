eb27870007098c49f1f052f1b354da82df54eaea
MarkusAleksander <markholden1989@googlemail.com>
2022-04-09 16:51:22 +0100

    Corrected env file location

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import './App.css';
+import { useState } from "react";
+import useFirebase from "./hooks/useFirebase";
 
-function App() {
+const App = () => {
+  const { writeToDb } = useFirebase();
+
+  const [result, setResult] = useState<string>("idle");
+
+  const writeToDbTest = () => {
+    setResult("loading...");
+
+    writeToDb("albums", {
+      album_id: "test",
+    })
+      .then(() => {
+        setResult("success");
+      })
+      .catch((err) => {
+        setResult(err);
+      });
+  };
+
   return (
     <div className="App">
-
+      <button onClick={writeToDbTest}>Write to DB</button>
+      <p>{result}</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,7 +4,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
