94505c4721959df74473cf88d2621ee767d97f7c
MarkusAleksander <markholden1989@googlemail.com>
2022-04-10 13:53:15 +0100

    Added routing

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,13 +1,20 @@
 import Header from "../UI/Header/Header";
 import Navigation from "../UI/Navigation/Navigation";
+import { NavLink } from "react-router-dom";
 
 const AppHeader = () => (
   <Header>
     <Navigation>
       <ul role="menu">
-        <li role="menuitem"></li>
-        <li role="menuitem"></li>
-        <li role="menuitem"></li>
+        <li role="menuitem">
+          <NavLink to="/">Search</NavLink>
+        </li>
+        <li role="menuitem">
+          <NavLink to="/artists">Artists</NavLink>
+        </li>
+        <li role="menuitem">
+          <NavLink to="/albums">Albums</NavLink>
+        </li>
       </ul>
     </Navigation>
   </Header>
